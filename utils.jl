fe599fe4456cc2d2d939d2481cd426ca540e96b8
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2022-12-02 23:46:00 -0500

    update talks

--- utils.jl
+++ utils.jl
@@ -1,9 +1,16 @@
 using YAML
 
 bibliography = YAML.load_file("data/bibliography.yaml")
+talks =  YAML.load_file("data/talks.yaml")
+talks_expanded = collect(talks)
+talk_names = first.(talks_expanded)
+talk_dates = map(x-> Date("$(x["year"])-$(x["month"])", "y-U"), last.(talks_expanded))
+_order = sortperm(talk_dates; rev=true)
+sorted_talks = talk_names[_order]
 
 icon_dict = Dict(
     "arXiv" => ("ai ai-arxiv ai-lg", "arXiv"),
+    "book" => ("las la-book la-lg", "Book chapter"),
     "openaccess" => ("ai ai-open-access ai-lg", "Journal paper"),
     "closedaccess" => ("ai ai-closed-access ai-lg", "Journal paper"),
     "conference" => ("ai ai-open-access ai-lg", "Conference paper"),
@@ -11,8 +18,13 @@ icon_dict = Dict(
     "reproduction_code" => ("las la-code la-lg", "Reproduction code"),
     "bioRxiv" => ("ai ai-biorxiv ai-lg", "bioRxiv"),
     "r_package" => ("lab la-r-project la-lg", "R package"),
-    "poster" => ("las la-chalkboard la-lg", "Poster")
+    "poster" => ("las la-chalkboard la-lg", "Poster"),
+    "video" => ("lab la-youtube la-lg", "Video"),
+    "slides" => ("las la-file-pdf la-lg", "Pdf slides"),
+    "link" => ("las la-external-link-alt la-lg", "Link")
 )
+
+
 
 
 function tooltip_code(url, class)
@@ -51,6 +63,7 @@ function bibprint(yamlkey)
     bibkey_order = ["openaccess";
                     "closedaccess";
                     "conference";
+                    "book";
                     "arXiv";
                     "bioRxiv";
                     "poster";
@@ -84,5 +97,54 @@ function hfun_bibliographyprint(yamlkeys)
     """
     $(html_string)
     </ol>
+    """
+end
+
+
+
+function talkprint(yamlkey)
+    entry = talks[yamlkey]
+
+    title = """<span class="title">$(entry["title"]).</span>"""
+    event = "$(entry["event"])"
+    date_location = "$(entry["month"]) $(entry["year"]), $(entry["location"])"
+
+    html_string = """
+    <li>
+    $(title)<br>
+    $(event)<br>
+    $(date_location)<br>
+    """
+
+    bibkey_order = ["link";
+                    "video";
+                    "slides"]
+    for key in bibkey_order
+        if haskey(entry, key)
+            html_string = """
+            $(html_string)
+            $(tooltip_code(entry[key], key))
+            """
+        end
+    end
+    html_string = """
+    $(html_string)
+    </li>
+    <br>
+    """;
+    html_string
+end
+
+function hfun_alltalksprint()
+    html_string = """<ul>"""
+    for key in sorted_talks
+        html_string = """
+        $(html_string)
+        $(talkprint(key))
+        """
+    end
+    """
+    $(html_string)
+    </ul>
     """
 end
